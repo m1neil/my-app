756dc51559536f33f4bd6dcd91e650f007b8e6af
Alekseyev Eric <ericalex0207@gmail.com>
2025-03-07 19:44:49 +0200

    init project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MyName from '../MyName/MyName'
+
+function App() {
+	return (
+		<div className='app'>
+			<div className='app-container'>
+				<h1>Привет React</h1>
+				<p>Это мой первый проект на React + Vite</p>
+				<MyName />
+			</div>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/MyName/MyName.jsx
@@ -0,0 +1,5 @@
+import './MyName.css'
+
+export default function MyName() {
+	return <h3 className="my-name">Меня зовут Эрик</h3>
+}
